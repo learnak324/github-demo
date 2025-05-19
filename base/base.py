b08a30517be99278ffa45e3bc21070a71d931660
ak <learnak2025@gmail.com>
2025-05-19 12:59:57 +0530

    sdet 2 created feature xyz

--- base/base.py
+++ base/base.py
@@ -1,7 +1,7 @@
 test file from sdedt 2
 
-test_file from sdet 1.
+test_file from sdet 1...
 
 test file updated by sdet 2 12:15PM
 
-sdet 2 create branch feature_xyz
+sdet 2 create branch feature_xyz....
--- testcase/testcase.py
+++ testcase/testcase.py
@@ -1,6 +1,6 @@
 print("test case1 from sdet2")
 
-print(Sdet 2 updated 12:05 PM)
+print(Sdet 2 updated 12:05 PM),,,,
 
 sdet 1 create branch feature _abc
 
